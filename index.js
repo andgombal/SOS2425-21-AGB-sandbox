c7572c4606a9bf0e5847fad13e951d9d74c0a6ed
andgombal <andgombal@alum.us.es>
2025-04-22 10:02:47 +0200

    .

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import { fileURLToPath } from "url";
 import { dirname } from "path";
 import { loadBackendAGB } from "./src/back/public-transit.js";
 
-import { handler } from './front/build/handler.js';
+import { handler } from '.src/front/build/handler.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
